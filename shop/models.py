841658742d1f994c8cd0a92f678eaa22f6d87143
Ramil-Nasibullin <ramil-hamza@yandex.ru>
2020-05-13 18:52:17 +0300

    сохранил все изменения

---
+++ shop/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+
+
+class Category(models.Model):
+    """Категории товаров"""
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Наименование')
+    slug = models.SlugField(max_length=200, unique=True, verbose_name='Уникальное значение')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """Товары"""
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE, verbose_name='Категория')
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Наименование')
+    slug = models.SlugField(max_length=200, db_index=True, verbose_name='Уникальное значение')
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True, verbose_name='Фото')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+    available = models.BooleanField(default=True, verbose_name='Наличие')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Обновлен')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        index_together = (('id', 'slug'),)
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
+
+    def __str__(self):
+        return self.name
